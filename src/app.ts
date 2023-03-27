b1e35eceeb42f985d96f9c4d32b04b6a81c16629
MesterMan03 <MesterMan03+github@proton.me>
2023-03-27 23:22:17 +0200

    added coinbase support changed product info structure

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
+import coinbase from "coinbase-commerce-node";
+import CC from "currency-converter-lt";
 import express from "express";
+import rateLimit from "express-rate-limit";
+import { readFileSync } from "fs";
+import { createServer } from "https";
+import { join } from "path";
 import { Stripe } from "stripe";
 import yup from "yup";
 import config from "./config.js";
 import productInfo from "./productInfo.js";
 import { verifyReq } from "./util.js";
-import rateLimit from "express-rate-limit";
-import CC from "currency-converter-lt";
-import { createServer } from "https";
-import { readFileSync } from "fs";
-import { join } from "path";
 
+const stripeClient = new Stripe(config.StripePrivateKey, { apiVersion: "2022-11-15" });
+coinbase.Client.init(config.CoinbaseKey);
 
 const app = express();
-const stripeClient = new Stripe(config.StripePrivateKey, { apiVersion: "2022-11-15" });
 const server = createServer({
     key: readFileSync(join("cert", "key.pem")),
     cert: readFileSync(join("cert", "cert.pem")),
@@ -31,43 +33,74 @@ app.use((err, req, res, next) => {
 })
 
 const checkoutSchema = yup.object({
-    body: yup.array().of(yup.object({
-        id: yup.string().required(),
-        quantity: yup.number().required()
-    })).required().min(1)
+    body: yup.object({
+        type: yup.string().required().ensure().oneOf(["stripe", "coinbase"]),
+        items: yup.array().of(yup.object({
+            id: yup.string().required(),
+            quantity: yup.number().required()
+        })).required().min(1)
+    }).required()
 })
 app.post("/create-checkout-session", verifyReq(checkoutSchema), async (req, res) => {
     try {
         // get the product info from the database
-        const rawProducts = req.body as { id: string, quantity: number }[];
-        const products = rawProducts.map(product => {
-            const realProduct = productInfo.find(p => p.id === product.id);
-            if (!realProduct) throw new Error("Invalid product id");
-            return {
-                price_data: {
-                    currency: "usd",
-                    unit_amount: realProduct.price,
-                    product_data: {
-                        name: realProduct.short_name,
-                        images: realProduct.images,
-                    }
+        const rawProducts = req.body.items as { id: string, quantity: number }[];
+
+        if (req.body.type === "stripe") {
+            const products = rawProducts.map(product => {
+                const realProduct = productInfo.find(p => p.id === product.id);
+                if (!realProduct) throw new Error("Invalid product id");
+                return {
+                    price_data: {
+                        currency: "usd",
+                        unit_amount: realProduct.price * 100,
+                        product_data: {
+                            name: realProduct.short_name,
+                            images: realProduct.images,
+                        }
+                    },
+                    quantity: product.quantity
+                } satisfies Stripe.Checkout.SessionCreateParams.LineItem
+            });
+
+            const session = await stripeClient.checkout.sessions.create({
+                payment_method_types: ["card"],
+                mode: "payment",
+                line_items: products,
+                success_url: `https://${config.DomainName}:${config.ServerPort}/success.html`,
+                cancel_url: `https://${config.DomainName}:${config.ServerPort}/cancel.html`,
+                shipping_address_collection: {
+                    allowed_countries: ["US"]
                 },
-                quantity: product.quantity
-            } satisfies Stripe.Checkout.SessionCreateParams.LineItem
-        });
+            });
 
-        const session = await stripeClient.checkout.sessions.create({
-            payment_method_types: ["card"],
-            mode: "payment",
-            line_items: products,
-            success_url: `https://${config.DomainName}:${config.ServerPort}/success.html`,
-            cancel_url: `https://${config.DomainName}:${config.ServerPort}/cancel.html`,
-            shipping_address_collection: {
-                allowed_countries: ["US", "HU"]
-            },
-        });
+            res.json({ url: session.url });
+        }
 
-        res.json({ url: session.url });
+        if (req.body.type === "coinbase") {
+            // calculate total price
+            const totalPrice = rawProducts.reduce((acc, product) => {
+                const realProduct = productInfo.find(p => p.id === product.id);
+                if (!realProduct) throw new Error("Invalid product id");
+                return acc + realProduct.price * product.quantity;
+            }, 0);
+
+            const chargeObj = new coinbase.resources.Charge({
+                name: "Openphones Order",
+                description: `Order for ${rawProducts.reduce((acc, curr) => acc + curr.quantity, 0)} items`,
+                pricing_type: "fixed_price",
+                local_price: {
+                    amount: totalPrice.toString(),
+                    currency: "USD"
+                },
+                redirect_url: `https://${config.DomainName}:${config.ServerPort}/success.html`,
+                cancel_url: `https://${config.DomainName}:${config.ServerPort}/cancel.html`
+            });
+
+            const charge = await chargeObj.save();
+
+            res.json({ url: charge.hosted_url });
+        }
 
     } catch (e) {
         res.status(500).send({ error: e.message });
@@ -84,7 +117,7 @@ app.get("/products", async (req, res) => {
             for (const product of productInfo) {
                 productInfoWithCurrency.push({
                     ...product,
-                    price: await converter.convert(product.price / 100)
+                    price: Number((await converter.convert(product.price) as number).toFixed(2))
                 });
             }
             return res.json(productInfoWithCurrency);
--- src/util.ts
+++ src/util.ts
@@ -8,7 +8,6 @@ export interface ProductInfo {
     images: string[];
     quality: "new" | "used";
     description: string;
-    available: number;
 }
 
 /**
